4c3cdf31632b57e18ff71a57a231e1f4fc0eacd3
Archerymystery <84961834+Archerymystery@users.noreply.github.com>
2026-06-19 17:37:32 +0200

    feat: add ability to nix run and nix build

--- flake.nix
+++ flake.nix
@@ -25,8 +25,32 @@
           "rust-analyzer"
         ];
       };
+      rustPlatform = pkgs.makeRustPlatform {
+        cargo = rustToolchain;
+        rustc = rustToolchain;
+      };
+      orderbookPkg = rustPlatform.buildRustPackage {
+        pname = "Orderbook";
+        version = "0.1.0";
+
+        src = ./.;
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+
+        nativeBuildInputs = with pkgs; [ pkg-config ];
+      };
     in
     {
+      packages.${system} = {
+        default = orderbookPkg;
+        Orderbook = orderbookPkg;
+      };
+      apps.${system}.default = {
+        type = "app";
+        program = "${orderbookPkg}/bin/Orderbook";
+      };
       devShells.x86_64-linux.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           rustToolchain
